a87e07872319f5864790438ea15c5bbc5b80b02b
Jordan Davidson <jordandavidson744@yahoo.com>
2020-02-07 16:54:41 -0500

    finished lists

--- list1.py
+++ list1.py
@@ -25,9 +25,12 @@
 
 
 def match_ends(words):
-    """Your code goes here.  Edit this docstring."""
-    return
-
+    count = 0
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            count += 1
+    return count
+            
 
 # B. front_x
 # Given a list of strings, return a list with the strings
@@ -37,8 +40,14 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    """Your code goes here.  Edit this docstring."""
-    return
+    x_first = []
+    not_x = []
+    for word in words:
+        if word.startswith('x'):
+            x_first.append(word)
+        else:
+            not_x.append(word)
+    return sorted(x_first) + sorted(not_x)
 
 
 # C. sort_last
@@ -48,9 +57,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    """Your code goes here.  Edit this docstring."""
-    return
-
+    return sorted(tuples, key = lambda a : a[-1])
+    
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
